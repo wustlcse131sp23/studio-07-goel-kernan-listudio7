236bccaf48509c50aaeafad7cfddadf31cde3d85
jase1 <jase1@LAPTOP-O9OBJN4Q.dhcp.wustl.edu>
2023-03-30 13:27:20 -0500

    j

---
+++ src/studio7/Shapes.java
@@ -0,0 +1,47 @@
+package studio7;
+
+public class Shapes {
+
+}
+
+class Rectangle {
+	private double height;
+	private double length;
+
+	public Rectangle(double height, double length) {
+		this.height = height;
+		this.length = length;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public double getArea() {
+		return getHeight() * getLength();
+	}
+
+	public double getPremimeter() {
+		return 2 * getHeight() + 2 * getLength();
+	}
+
+	public boolean isSquare() {
+		return getHeight() == getLength();
+	}
+}
+
+class Die {
+	private int sides;
+
+	public Die(int sides) {
+		this.sides = sides;
+	}
+
+	public int rollDie() {
+		return (int) Math.floor(Math.random() * sides) + 1;
+	}
+}
